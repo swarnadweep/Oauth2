581d09b89c1a14e9bd46dcac563b212fdebb861f
swarnadweep panja <swarnadweeppanja@gmail.com>
2026-03-12 23:21:20 +0530

    Oath

---
+++ src/main/java/com/example/Oauth2/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.Oauth2;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(authReq ->
+                authReq.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
